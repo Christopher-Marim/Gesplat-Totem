738e0c7402dc333ad149320fd824c7d273a73ee7
Christopher Marim <cssmarim1@gmail.com>
2021-12-25 17:31:58 -0300

    Inicio projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./hooks/auth";
+import { StateProvider } from "./hooks/state";
+import { HomeScreen } from "./screens/HomeScreen/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <StateProvider>
+          <Routes>
+            <Route element={<HomeScreen />} path="/" />
+            <Route element={<HomeScreen />} path="/pages/home" />
+          </Routes>
+        </StateProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  width:100%;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background:linear-gradient(#fff, #cccccc);
+  
+`;
