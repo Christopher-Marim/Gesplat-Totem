a4a5d243502566ffd1e157f5327c2101cd3b71c7
Christopher Marim <cssmarim1@gmail.com>
2022-01-03 20:09:46 -0300

    Step screen

--- src/screens/ProcessScreen/index.tsx
+++ src/screens/ProcessScreen/index.tsx
@@ -1,24 +1,72 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
-import { Container } from "./styles";
+import { Container, Procedimento } from "./styles";
 
 export function ProcessScreen(){
     const navigate = useNavigate();
+
+    const array=[
+        {
+            id:'1',
+            step:'Antropometria',
+            level:'1',
+            position:'5',
+            fila_tipo:'F',
+            fila_number:'03'
+        },
+        {
+            id:'2',
+            step:'TESTE DE BOCA',
+            level:'1',
+            position:'2',
+            fila_tipo:'F',
+            fila_number:'03'
+        },
+        {
+            id:'3',
+            step:'Teste de olho',
+            level:'1',
+            position:'7',
+            fila_tipo:'F',
+            fila_number:'03'
+        },
+        {
+            id:'3',
+            step:'Teste de olho',
+            level:'1',
+            position:'7',
+            fila_tipo:'F',
+            fila_number:'03'
+        },
+        {
+            id:'3',
+            step:'Teste de olho',
+            level:'1',
+            position:'7',
+            fila_tipo:'F',
+            fila_number:'03'
+        },
+    ]
 
     return(
         <Container>
                 <h1>PROCEDIMENTOS<br></br> PARA HOJE</h1>
                 <div className='procedimentos'>
-                    <div>
-                        <div>
-                        <h3>ANTROPOMETRIA</h3>
-                        <p>Andar 1</p>
-                        <p>Posição 5</p>
-                        </div>
-                        <label><strong>F</strong>03</label>
-                    </div>
+                    {array.map((item)=>(
+                         <Procedimento key={item.id}>
+                         <div>
+                         <h3>{item.step}</h3>
+                         <p>Andar <strong>{item.level}</strong></p>
+                         <p>Posição <strong>{item.position}</strong></p>
+                         </div>
+                         <label><strong>{item.fila_tipo}</strong>{item.fila_number}</label>
+                     </Procedimento>
+                    ))}
+                   
                 </div>
-                <Button onClick={()=>navigate('/finish')}><label>Prosseguir</label></Button>
+               
+                <Button style={{maxWidth:800}}onClick={()=>navigate('/finish')}><label>Prosseguir</label></Button>
+                
         </Container>
     )
 }
--- src/screens/ProcessScreen/styles.ts
+++ src/screens/ProcessScreen/styles.ts
@@ -8,40 +8,77 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
 
-  h1{
-  font-size:35px;
-  letter-spacing: 2px;
-  margin:0px 40px;
-  margin-top:40px;
-      text-align:center;
-      font-weight:400;
-      strong{
-          color:#9CECF0
-      }
+  h1 {
+    font-size: 35px;
+    letter-spacing: 6px;
+    line-height: 41px;
+    margin: 0px 40px;
+    margin-top: 40px;
+    text-align: center;
+    font-weight: 500;
   }
 
-  .procedimentos{
-      display:flex;
-      flex-direction:column;
-      justify-content:center;
-      align-items:center;
-      height:600px;
-      width:70%;
-      background:red;
+  .procedimentos {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    height: 600px;
+    width: 70%;
+    max-width: 800px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    
+    ::-webkit-scrollbar {
+  width: 10px;
+  
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background:linear-gradient( white , #F09C9C );
+  border-radius: 10px;
+  
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  transition: 500ms ease-in-out;
+  background: #F09C9C; 
+  
+}
   }
 `;
-
-export const Name = styled.h3`
-  margin: 10px 0px;
-  font-size:25px;
-  font-family: "Dosis", sans-serif;
-  color: #00c2cb;
-`;
-
-export const WrapperQRCODE = styled.div`
+export const Procedimento = styled.div`
   display: flex;
-  justify-content: center;
-  flex-direction: column;
+  width: 100%;
+  justify-content: space-between;
   align-items: center;
-  margin-bottom:40px
+  background: transparent;
+  border: 2px solid white;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 5px;
+
+  h3 {
+    font-family: "Inter";
+    font-size: 25px;
+    text-transform: uppercase;
+    letter-spacing: 4px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin: 0px 10px;
+    font-size: 19px;
+    letter-spacing: 2px;
+  }
+
+  label {
+    font-size: 35px;
+    letter-spacing: 4px;
+  }
+  strong {
+    color: #f09c9c;
+  }
 `;
