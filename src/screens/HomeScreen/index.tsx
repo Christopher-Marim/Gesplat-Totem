7acaefc2c01b10643fa29a7a6ce3183bc0bd22a4
Christopher Marim <cssmarim1@gmail.com>
2022-01-08 14:12:39 -0300

    masck cpf

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -3,15 +3,37 @@ import { WebcamComponent } from "../../components/Cam";
 import { QrCodeComponent } from "../../components/QrCode";
 import { Logo } from "../../components/LogoComponent";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { maskCpf } from "../../utils/mask";
 
 export function HomeScreen() {
   const navigate = useNavigate();
+
+  const [valor, setValor] = useState("");
+  const [valorAux, setValorAux] = useState("");
+
+  function handleChangeMask(event:any) {
+    const { value } = event.target;
+    var v = value.replace(/\./g, '')  
+    setValorAux(v.replace(/\-/g, ''));
+
+    setValor(maskCpf(value));
+  }
   return (
     <Container>
       <Logo></Logo>
       <WrapperCpf>
         <label>DIGITE SEU CPF</label>
-        <input inputMode='numeric' placeholder="123.456.789-10" onClick={()=>{navigate('user')}}></input>
+        <input
+          inputMode="numeric"
+          placeholder="123.456.789-10"
+          onChange={handleChangeMask}
+          maxLength={14}
+          value={valor}
+          /* onClick={() => {
+            navigate("user");
+          }} */
+        ></input>
       </WrapperCpf>
       <h3>OU</h3>
       <WrapperQRCODE>
